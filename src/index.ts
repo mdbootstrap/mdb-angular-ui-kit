71f052351eb1e463a8272f90fde187904e1b6e2e
Chris McKnight <cmckni3@gmail.com>
2017-11-07 00:13:44 -0600

    build(compile): Compile components
    Compile TypeScript and Angular using ngc and ts-node
    
    Close #20

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,8 @@ import { RippleModule } from './ripple';
 import { ActiveModule } from './inputs';
 import { NavbarModule } from './navbars';
 import { BsDropdownModule } from './dropdown';
-import { CarouselModule } from './carousel/';
-import { MDBChartsModule } from './charts/';
+import { CarouselModule } from './carousel';
+import { MDBChartsModule } from './charts';
 import { CollapseModule } from './collapse';
 import { ModalModule } from './modals';
 import { MDBTooltipModule } from './tooltip';
@@ -15,49 +15,49 @@ import { PopoverModule } from './popover';
 
 export {
   ButtonsModule, ButtonRadioDirective, ButtonCheckboxDirective
-} from './buttons/';
+} from './buttons';
 
 export {
   RippleModule, RippleDirective
-} from './ripple/';
+} from './ripple';
 
 export {
   ActiveModule, ActiveDirective, InputValidateDirective
-} from './inputs/';
+} from './inputs';
 
 export {
   NavbarModule
-} from './navbars/';
+} from './navbars';
 
 export {
   BsDropdownConfig, BsDropdownContainerComponent, BsDropdownDirective, BsDropdownMenuDirective,
   BsDropdownModule, BsDropdownState, BsDropdownToggleDirective
-} from './dropdown/';
+} from './dropdown';
 
 export {
   CarouselComponent, CarouselConfig, CarouselModule
-} from './carousel/';
+} from './carousel';
 
 export {
   MDBChartsModule, BaseChartDirective
-} from './charts/';
+} from './charts';
 
 export {
   CollapseDirective, CollapseModule
-} from './collapse/';
+} from './collapse';
 
 export {
   ModalBackdropComponent, ModalBackdropOptions, ModalDirective, ModalModule, ModalOptions, MDBModalService,
   ModalContainerComponent, MDBModalRef
-} from './modals/';
+} from './modals';
 
 export {
   TooltipConfig, TooltipContainerComponent, TooltipDirective, MDBTooltipModule
-} from './tooltip/';
+} from './tooltip';
 
 export {
   PopoverConfig, PopoverContainerComponent, PopoverModule, PopoverDirective
-} from './popover/';
+} from './popover';
 
 
 
