8535b818bbdbb5d78c63beee186f8a4f1ca86c55
Damian <d.gemza@mdbootstrap.com>
2017-12-20 15:27:01 +0100

    Fixed problems with navbar on mobile

---
+++ src/navbars/logo.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'logo',
+  template: `
+  <ng-content></ng-content>
+  `
+})
+export class LogoComponent {
+
+}
--- src/navbars/navbar.component.ts
+++ src/navbars/navbar.component.ts
@@ -1,30 +1,17 @@
-import { Component, ElementRef , ViewChild, Input,  Renderer, AfterViewInit, HostListener } from '@angular/core';
+import { NavbarService } from './navbar.service';
+import { Component, ElementRef, ViewChild, Input, Renderer, AfterViewInit, HostListener } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
-@Component ({
+@Component({
   selector: 'mdb-navbar',
-  template: `
-  <nav class="{{SideClass}}" #nav>
-  <ng-content></ng-content>
-  <div [ngClass]="{'container': containerInside}" [ngStyle]="{'display': displayStyle}" #container>
-    <ng-content select="logo"></ng-content>
-    <button class="navbar-toggler" type="button" 
-    (click)="toggle($event)" mdbRippleRadius
-    *ngIf="this.el.nativeElement.children.length !== 0">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div #navbar [style.height]="height" class="navbar-collapse collapse" 
-    [ngClass]="{'collapse': collapse, 'show': showClass, 'collapsing': collapsing}">
-      <ng-content select="links" ></ng-content>
-    </div>
-  </div>
-</nav>
-
-  `
+  templateUrl: 'navbar.component.html',
 })
 
 export class NavbarComponent implements AfterViewInit {
   @Input() SideClass: string;
   @Input() containerInside = true;
+  subscription: Subscription;
+  navbarLinkClicks: any;
   shown = false;
 
   public height: number;
@@ -39,7 +26,18 @@ export class NavbarComponent implements AfterViewInit {
   @ViewChild('nav') navbar: ElementRef;
   @ViewChild('container') container: ElementRef;
 
-  constructor(public renderer: Renderer) {}
+  constructor(public renderer: Renderer, private _navbarService: NavbarService) {
+    // tslint:disable-next-line:max-line-length
+    this.subscription = this._navbarService.getNavbarLinkClicks().subscribe(navbarLinkClicks => { this.closeNavbarOnClick(navbarLinkClicks); });
+  }
+
+  closeNavbarOnClick(navbarLinkClicks: any) {
+    this.navbarLinkClicks = navbarLinkClicks;
+    if (this.showClass) {
+      this.hide();
+    }
+  }
+
 
   ngAfterViewInit() {
     /* bugfix - bez tego sypie ExpressionChangedAfterItHasBeenCheckedError -
@@ -80,7 +78,7 @@ export class NavbarComponent implements AfterViewInit {
     this.collapse = false;
     this.collapsing = true;
     setTimeout(() => {
-      this.renderer.setElementStyle(this.el.nativeElement, 'height',  this.height + 'px');
+      this.renderer.setElementStyle(this.el.nativeElement, 'height', this.height + 'px');
     }, 10);
 
 
@@ -97,7 +95,7 @@ export class NavbarComponent implements AfterViewInit {
     this.showClass = false;
     this.collapsing = true;
     setTimeout(() => {
-      this.renderer.setElementStyle(this.el.nativeElement, 'height',  '0px');
+      this.renderer.setElementStyle(this.el.nativeElement, 'height', '0px');
     }, 10);
 
 
@@ -109,56 +107,54 @@ export class NavbarComponent implements AfterViewInit {
 
   get displayStyle() {
     // if(!this.containerInside) {
-      //  return 'flex';
-      // } else {
-        return '';
-        // }
-      }
+    //  return 'flex';
+    // } else {
+    return '';
+    // }
+  }
 
-      @HostListener('window:resize', ['$event']) onResize(event: any) {
-        let breakpoit = 0;
+  @HostListener('window:resize', ['$event']) onResize(event: any) {
+    let breakpoit = 0;
 
-        if ( this.SideClass.includes('navbar-expand-xl') ) {
-          breakpoit = 1200;
-        } else if (this.SideClass.includes('navbar-expand-lg')) {
-          breakpoit = 992;
-        } else if (this.SideClass.includes('navbar-expand-md')) {
-          breakpoit = 768;
-        } else if (this.SideClass.includes('navbar-expand-sm')) {
-          breakpoit = 576;
-        } else {
-          breakpoit = event.target.innerWidth + 1;
-        }
+    if (this.SideClass.includes('navbar-expand-xl')) {
+      breakpoit = 1200;
+    } else if (this.SideClass.includes('navbar-expand-lg')) {
+      breakpoit = 992;
+    } else if (this.SideClass.includes('navbar-expand-md')) {
+      breakpoit = 768;
+    } else if (this.SideClass.includes('navbar-expand-sm')) {
+      breakpoit = 576;
+    } else {
+      breakpoit = event.target.innerWidth + 1;
+    }
 
-        // console.log(breakpoit);
-
-        if (event.target.innerWidth < breakpoit) {
-          if (!this.shown) {
-            this.collapse = false;
-            this.renderer.setElementStyle(this.el.nativeElement, 'height',  '0px');
-            this.renderer.setElementStyle(this.el.nativeElement, 'opacity',  '0');
-            setTimeout(() => {
-              this.height = this.el.nativeElement.scrollHeight;
-              this.collapse = true;
-              this.renderer.setElementStyle(this.el.nativeElement, 'opacity',  '');
-            }, 4);
-          }
-        } else {
-          this.collapsing = false;
-          this.shown = false;
-          this.showClass = false;
+    if (event.target.innerWidth < breakpoit) {
+      if (!this.shown) {
+        this.collapse = false;
+        this.renderer.setElementStyle(this.el.nativeElement, 'height', '0px');
+        this.renderer.setElementStyle(this.el.nativeElement, 'opacity', '0');
+        setTimeout(() => {
+          this.height = this.el.nativeElement.scrollHeight;
           this.collapse = true;
-          this.renderer.setElementStyle(this.el.nativeElement, 'height',  '');
-        }
+          this.renderer.setElementStyle(this.el.nativeElement, 'opacity', '');
+        }, 4);
       }
+    } else {
+      this.collapsing = false;
+      this.shown = false;
+      this.showClass = false;
+      this.collapse = true;
+      this.renderer.setElementStyle(this.el.nativeElement, 'height', '');
+    }
+  }
 
-      @HostListener('document:scroll', ['$event']) onScroll() {
-        if (this.navbar.nativeElement.classList.contains('scrolling-navbar')) {
-          if (window.pageYOffset > 120) {
-            this.renderer.setElementClass(this.navbar.nativeElement, 'top-nav-collapse', true);
-          } else {
-            this.renderer.setElementClass(this.navbar.nativeElement, 'top-nav-collapse', false);
-          }
-        }
+  @HostListener('document:scroll', ['$event']) onScroll() {
+    if (this.navbar.nativeElement.classList.contains('scrolling-navbar')) {
+      if (window.pageYOffset > 120) {
+        this.renderer.setElementClass(this.navbar.nativeElement, 'top-nav-collapse', true);
+      } else {
+        this.renderer.setElementClass(this.navbar.nativeElement, 'top-nav-collapse', false);
       }
     }
+  }
+}
