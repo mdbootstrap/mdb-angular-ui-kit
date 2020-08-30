e5a29925eea750c1d8f25f8b8adc903064ff2f8c
a.idzikowski <a.idzikowski@mdbootstrap.com>
2020-08-30 21:48:30 +0200

    Release 9.4.0

--- projects/angular-bootstrap-md/src/lib/free/dropdown/dropdown-toggle.directive.ts
+++ projects/angular-bootstrap-md/src/lib/free/dropdown/dropdown-toggle.directive.ts
@@ -1,4 +1,12 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnDestroy } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  OnDestroy,
+  Renderer2,
+  ChangeDetectorRef,
+} from '@angular/core';
 import { Subscription } from 'rxjs';
 
 import { BsDropdownState } from './dropdown.state';
@@ -9,6 +17,8 @@ import { BsDropdownState } from './dropdown.state';
 })
 export class BsDropdownToggleDirective implements OnDestroy {
   private _subscriptions: Subscription[] = [];
+  private _documentClickListener: Function;
+  private _escKeyUpListener: Function;
 
   @HostBinding('attr.aria-haspopup') ariaHaspopup = true;
   @HostBinding('attr.disabled') isDisabled: boolean | any = null;
@@ -22,29 +32,43 @@ export class BsDropdownToggleDirective implements OnDestroy {
     this._state.toggleClick.emit();
   }
 
-  @HostListener('document:click', ['$event'])
-  onDocumentClick(event: any): void {
-    if (
-      this._state.autoClose &&
-      event.button !== 2 &&
-      !this._element.nativeElement.contains(event.target)
-    ) {
-      this._state.toggleClick.emit(false);
-    }
-  }
-
-  @HostListener('keyup.esc')
-  onEsc(): void {
-    if (this._state.autoClose) {
-      this._state.toggleClick.emit(false);
-    }
-  }
-
-  constructor(private _state: BsDropdownState, private _element: ElementRef) {
+  constructor(
+    private _state: BsDropdownState,
+    private _element: ElementRef,
+    private _renderer: Renderer2,
+    private _cdRef: ChangeDetectorRef
+  ) {
     // sync is open value with state
-    this._subscriptions.push(
-      this._state.isOpenChange.subscribe((value: boolean) => (this.isOpen = value))
-    );
+    this._state.isOpenChange.subscribe((value: boolean) => {
+      this.isOpen = value;
+
+      if (value) {
+        this._documentClickListener = this._renderer.listen('document', 'click', (event: any) => {
+          if (
+            this._state.autoClose &&
+            event.button !== 2 &&
+            !this._element.nativeElement.contains(event.target)
+          ) {
+            this._state.toggleClick.emit(false);
+            this._cdRef.detectChanges();
+          }
+        });
+
+        this._escKeyUpListener = this._renderer.listen(
+          this._element.nativeElement,
+          'keyup.esc',
+          () => {
+            if (this._state.autoClose) {
+              this._state.toggleClick.emit(false);
+              this._cdRef.detectChanges();
+            }
+          }
+        );
+      } else {
+        this._documentClickListener();
+        this._escKeyUpListener();
+      }
+    });
     // populate disabled state
     this._subscriptions.push(
       this._state.isDisabledChange.subscribe(
@@ -54,6 +78,14 @@ export class BsDropdownToggleDirective implements OnDestroy {
   }
 
   ngOnDestroy(): void {
+    if (this._documentClickListener) {
+      this._documentClickListener();
+    }
+
+    if (this._escKeyUpListener) {
+      this._escKeyUpListener();
+    }
+
     for (const sub of this._subscriptions) {
       sub.unsubscribe();
     }
